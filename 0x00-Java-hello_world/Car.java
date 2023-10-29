2f1cd2f2bd09d031bcb0709e978faf64aafbadcf
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-29 22:56:32 +0100

    Update File Car.java

--- 0x00-Java-hello_world/Car.java
+++ 0x00-Java-hello_world/Car.java
@@ -2,6 +2,12 @@
 public class Car {
     public static void main(String []args){
         String carName = "Volvo";
-        System.out.println("The name of car is " +carName);
+        int maxSpeed = 120;
+        int x = 5;
+        int y = 10;
+        System.out.println("The name of car is " + carName + "and its max speed is " +maxSpeed + "KM/h");
+        System.out.println(x + y);
+        int z = x + y;
+        System.out.println(z);
     }
 }
