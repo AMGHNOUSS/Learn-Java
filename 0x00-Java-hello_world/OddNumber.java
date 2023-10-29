ddb4f7014b4c168ce17b1de19f044d860bf6f369
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-29 23:36:43 +0100

    Fix Style coding

--- 0x00-Java-hello_world/OddNumber.java
+++ 0x00-Java-hello_world/OddNumber.java
@@ -5,7 +5,7 @@ public class OddNumber {
         // This function convert a string to integer.
         int num = Integer.parseInt(args[0]);
         if (num % 2 == 0){
-            System.out.println("The number " + num + "is even.");
+            System.out.println("The number " + num + " is even.");
         }
         else {
             System.out.println("The number " + num + " is odd.");
