b4d2c4a24de00cdf8c504763fb455f10aefadc87
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-29 23:11:36 +0100

    Create File DataType

---
+++ 0x00-Java-hello_world/DataType.java
@@ -0,0 +1,12 @@
+
+public class DataType {
+    public static void main(String[] args) {
+        int myNum = 9;
+        float myFloatNum = 8.999f;
+        char myLetter = 'A';
+        boolean myBool = false;
+        String myText = "Hello World";
+        System.out.print(myNum + " " + myFloatNum + " " + myLetter + " ");
+        System.out.println(myBool + " " + myText + ".");
+    }
+}
