281d4b48c0a7f08066e7057bfe53b575d3612257
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-30 12:37:40 +0100

    Narrowing casting

--- 0x00-Java-hello_world/DataType.java
+++ 0x00-Java-hello_world/DataType.java
@@ -1,12 +1,18 @@
 
 public class DataType {
     public static void main(String[] args) {
-        int myNum = 9;
+        byte  myByte = 15; // 1 byte = 8 bit
+        int myNum = 5000000; // 4 byte = (8 * 4)byte
         float myFloatNum = 8.999f;
         char myLetter = 'A';
         boolean myBool = false;
         String myText = "Hello World";
-        System.out.print(myNum + " " + myFloatNum + " " + myLetter + " ");
+        System.out.print(myByte + " " + myNum + " " + myFloatNum + " " + myLetter + " ");
         System.out.println(myBool + " " + myText + ".");
+
+        // Narrowing casting
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
     }
 }
