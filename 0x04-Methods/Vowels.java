5ae802fff6a4a753fbf19a3ad1fc5addb33e59ca
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-01 19:38:17 +0100

    Create File Vowels.java

---
+++ 0x04-Methods/Vowels.java
@@ -0,0 +1,21 @@
+
+// count all vowels in a string at given i narguments
+
+public class Vowels 
+{
+    public static void main(String[] args) {
+        System.out.print("Number of Vowels in the string: " + CountVowels(args[0]));
+    }
+    
+    public static int CountVowels(String str) {
+        String strV = "aeiou";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < strV.length(); j++) {
+                if (str.charAt(i) == strV.charAt(j))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
