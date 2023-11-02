8dd7b5a7e6edda7aa8f72a599429262a4c8f43c2
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-02 21:26:36 +0100

    Update File ReverseWord.java

--- 0x05-Strings/ReverseWord.java
+++ 0x05-Strings/ReverseWord.java
@@ -1,16 +1,28 @@
 
 // Reverse every word in a string using methods.
+/*
+ * Given: The given string is: This is a test string
+ * Exepcted: The string reversed word by word is: sihT si a tset gnirts
+ */
+
+import java.util.Scanner;
 
 public class ReverseWord {
     public static void main(String[] args) {
         
-        String txt = args[0];
-        System.out.println("The given string is: " + txt);
-        System.out.print("The string reversed word by word is: ");
-        reverseEachWordInString(txt); 
+        try (Scanner obj = new Scanner(System.in)) {
+            System.out.println("The given string is: ");
+            String txt = obj.nextLine();
+            System.out.print("The string reversed word by word is: ");
+            String[] arr = txt.split(" ");
+            for (int i = 0; i < arr.length; i++) {
+                reverseWord(arr[i]);
+                System.out.print(" ");
+            }
+        } 
     }
 
-    public static void reverseEachWordInString(String txt){
+    public static void reverseWord(String txt){
         String reversedStr = "";
         for (int i = 0; i < txt.length(); i++) {
             reversedStr = txt.charAt(i) + reversedStr;
