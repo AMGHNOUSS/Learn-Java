40aaf55b53534a8bc8214196526129b86fc41ef3
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-02 21:45:35 +0100

    Create File Palindrome.java

---
+++ 0x05-Strings/Palindrome.java
@@ -0,0 +1,25 @@
+
+// checks whether a given string is a palindrome or not.
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String txt;
+        System.out.print("Enter Your Text : ");
+        txt = obj.nextLine();
+        String revtxt = reverseWord(txt);
+        if (txt.equals(revtxt))
+            System.out.println(txt + " is palindrome!.");
+        else
+            System.out.println(txt + " is not palindrome!.");
+    }
+    public static String reverseWord(String txt){
+        String reversedStr = "";
+        for (int i = 0; i < txt.length(); i++) {
+            reversedStr = txt.charAt(i) + reversedStr;
+        }
+        return reversedStr;
+    }
+}
