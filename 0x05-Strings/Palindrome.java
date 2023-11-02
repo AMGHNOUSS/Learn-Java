687c400dc8f8ac0117e8ba5bc920325a5eadf04a
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-02 21:46:46 +0100

    Remove all file .class

--- 0x05-Strings/Palindrome.java
+++ 0x05-Strings/Palindrome.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        String txt;
-        System.out.print("Enter Your Text : ");
-        txt = obj.nextLine();
-        String revtxt = reverseWord(txt);
-        if (txt.equals(revtxt))
-            System.out.println(txt + " is palindrome!.");
-        else
-            System.out.println(txt + " is not palindrome!.");
+        try (Scanner obj = new Scanner(System.in)) {
+            String txt;
+            System.out.print("Enter Your Text : ");
+            txt = obj.nextLine();
+            String revtxt = reverseWord(txt);
+            if (txt.equals(revtxt))
+                System.out.println(txt + " is palindrome!.");
+            else
+                System.out.println(txt + " is not palindrome!.");
+        }
     }
     public static String reverseWord(String txt){
         String reversedStr = "";
