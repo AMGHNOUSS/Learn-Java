3035f5b6362e300603d9bcabc5ba24847289c1a5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-03 14:58:54 +0100

    Create File Colors.java

---
+++ 0x06-ArrayList/Colors.java
@@ -0,0 +1,17 @@
+
+// package 0x06-ArrayList;
+
+import java.util.ArrayList;
+
+public class Colors {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<String>();
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("Green");
+        colors.add("Gray");
+        for (String string : colors) {
+            System.out.println(string);
+        }
+    }
+}
