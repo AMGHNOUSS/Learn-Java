1573d876aa4965c2adc23346a99e9b073213a6f2
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-03 15:33:40 +0100

    Create Function update()

--- 0x06-ArrayList/Languages.java
+++ 0x06-ArrayList/Languages.java
@@ -19,12 +19,16 @@ public class Languages {
             System.out.print(i + " ");
         }
         System.out.println("\nThe value of index 3 : " + Retrieve(MyNumbers, 3));
+        System.out.println("Update value of index 4 by 8 " + update(MyNumbers, 8, 4));
         for (int i : MyNumbers) {
             System.out.print(i + " ");
         }
     }
     static int Retrieve(ArrayList<Integer> arr, int n) {
         return arr.get(n);
+    }
+    static int update(ArrayList<Integer> arr, int valueNew, int n) {
+        return arr.set(n, valueNew);
     }
     
 }
