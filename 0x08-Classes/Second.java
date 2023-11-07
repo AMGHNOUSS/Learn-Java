723a962af219590a0ae63d7096573e917667d935
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-07 20:10:50 +0100

    Create class Person

--- 0x08-Classes/Second.java
+++
@@ -1,8 +0,0 @@
-
-// Class Second
-public class Second {
-    public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println(myObj.x);
-    }
-}
---
+++ 0x08-Classes/Testing.java
@@ -0,0 +1,12 @@
+
+// We all test our class here.
+public class Testing {
+    public static void main(String[] args) {
+        
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+        
+        Person p = new Person("Redouane", 21);
+        p.method();
+    }    
+}
