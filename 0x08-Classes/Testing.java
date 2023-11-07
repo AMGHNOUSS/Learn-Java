1322566a238940af7449c7c57b2f64ed14235794
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-11-07 20:31:27 +0100

    Create class dog

--- 0x08-Classes/Testing.java
+++ 0x08-Classes/Testing.java
@@ -8,5 +8,14 @@ public class Testing {
         
         Person p = new Person("Redouane", 21);
         p.method();
+
+        Dog d1 = new Dog("Max", "Rottweiler");
+        Dog d2 = new Dog("Fred", "German");
+        d1.setName("Fred");
+        d2.setName("Max");
+        d1.setBreed("German");
+        d2.setBreed("Rottweiler");
+        d1.print();
+        d2.print();
     }    
 }
